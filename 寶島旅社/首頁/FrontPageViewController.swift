3f476c060076141eb8155a6f8102516855990aa0
EricYou <zl2408399@gmail.com>
2023-07-16 10:21:13 +0800

    feat: homePage hidden navigation

--- 寶島旅社/首頁/FrontPageViewController.swift
+++ 寶島旅社/首頁/FrontPageViewController.swift
@@ -97,7 +97,16 @@ class FrontPageViewController: UIViewController {
         
 //        topButtonView.isHidden = true
     }
-   
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
     
     //MARK: - searchBar相關
     @IBAction func searchBtn(_ sender: Any) {
