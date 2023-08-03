1a9cf554c2e95c6e3e29fb0bc9594adf1eeb88b6
EricYou <zl2408399@gmail.com>
2023-08-03 15:38:14 +0800

    feat: ImageDataCollectionViewCell 改用 code 做

--- 寶島旅社/首頁/旅店獨立介紹頁/Cell/CollectionViewCell/ImageDataCollectionViewCell.swift
+++ 寶島旅社/首頁/旅店獨立介紹頁/Cell/CollectionViewCell/ImageDataCollectionViewCell.swift
@@ -9,18 +9,86 @@
 import UIKit
 
 class ImageDataCollectionViewCell: UICollectionViewCell {
-    @IBOutlet weak var hotelImageView: UIImageView!
-    @IBOutlet weak var titleLabel: UILabel! {
-        didSet {
-            //圓角
-            titleLabel.layer.cornerRadius = 15
-            titleLabel.layer.masksToBounds = true
+    //MARK: - UI
+    private let hotelImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.textColor = .mainRed
+        label.font = .boldSystemFont(ofSize: 17)
+        label.textAlignment = .center
+        
+        //圓角
+        label.layer.cornerRadius = 15
+        label.layer.masksToBounds = true
+        
+        //邊框線條
+        label.layer.borderWidth = 2
+        label.layer.borderColor = UIColor.mainGreen.cgColor
+        
+        label.backgroundColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSubView()
+        setupConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - setup
+    private func setupSubView() {
+        addSubview(hotelImageView)
+        addSubview(titleLabel)
+    }
+    
+    private func setupConstraint() {
+        NSLayoutConstraint.activate([
+            hotelImageView.topAnchor.constraint(equalTo: topAnchor),
+            hotelImageView.leftAnchor.constraint(equalTo: leftAnchor),
+            hotelImageView.rightAnchor.constraint(equalTo: rightAnchor),
             
-            //邊框線條
-            titleLabel.layer.borderWidth = 2
-            titleLabel.layer.borderColor = UIColor.mainGreen.cgColor
-            
-            titleLabel.backgroundColor = UIColor.white
+            titleLabel.topAnchor.constraint(equalTo: hotelImageView.bottomAnchor, constant: 10),
+            titleLabel.leftAnchor.constraint(equalTo: hotelImageView.leftAnchor),
+            titleLabel.rightAnchor.constraint(equalTo: hotelImageView.rightAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+            titleLabel.heightAnchor.constraint(equalToConstant: 40),
+        ])
+    }
+    
+    func configure(with imageURL: String? = nil, title: String? = nil) {
+        // Set hotel image
+        if let imageURL = imageURL, let url = URL(string: imageURL) {
+            DispatchQueue.global().async {
+                if let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self.hotelImageView.image = image
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        self.hotelImageView.loadGif(name: "嘎喔兔兔")
+                    }
+                }
+            }
+        } else {
+            self.hotelImageView.loadGif(name: "嘎喔兔兔")
+        }
+        
+        // Set title label
+        if let title = title {
+            titleLabel.text = title.isEmpty ? "實景示意圖" : title
+        } else {
+            titleLabel.text = "很抱歉，此旅店尚未提供圖檔哦~"
         }
     }
 }
