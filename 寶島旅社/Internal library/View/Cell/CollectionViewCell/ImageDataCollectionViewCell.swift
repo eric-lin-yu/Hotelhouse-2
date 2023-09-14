e642db0b10bb63b3be2ce412961f365cfd8af061
EricLin <zl2408399@gmail.com>
2023-09-14 14:13:33 +0800

    feat: add hotelimage 說明.count > 15，可開啟 popupView

--- 寶島旅社/Internal library/View/Cell/CollectionViewCell/ImageDataCollectionViewCell.swift
+++ 寶島旅社/Internal library/View/Cell/CollectionViewCell/ImageDataCollectionViewCell.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class ImageDataCollectionViewCell: UICollectionViewCell {
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSubView()
+        setupConstraint()
+    }
+    
     //MARK: - UI
     private let hotelImageView: UIImageView = {
         let imageView = UIImageView()
@@ -37,34 +47,85 @@ class ImageDataCollectionViewCell: UICollectionViewCell {
         return label
     }()
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupSubView()
-        setupConstraint()
-    }
+    private let popupView: UIView = {
+        let view = UIView()
+        view.roundFrameView(roundView: view)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    private let popupTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = .systemFont(ofSize: 16)
+        textView.isEditable = false
+        textView.isScrollEnabled = true
+        textView.textAlignment = .center
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    private let closeButton: UIButton = {
+        let button = UIButton()
+        let image = UIImage(named: "iconClosed(R)")
+        button.setImage(image, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     //MARK: - setup
     private func setupSubView() {
-        addSubview(hotelImageView)
-        addSubview(titleLabel)
+        let viewsToAdd: [UIView] = [
+            hotelImageView,
+            titleLabel,
+            popupView,
+        ]
+        viewsToAdd.forEach { contentView.addSubview($0) }
+        
+        let viewsToAddPopupView: [UIView] = [
+            popupTextView,
+            closeButton,
+        ]
+        viewsToAddPopupView.forEach { popupView.addSubview($0) }
+        popupView.isHidden = true
     }
     
     private func setupConstraint() {
+        let topContentViewAnchor = contentView.topAnchor
+        let leftContentViewAnchor = contentView.leftAnchor
+        let rightContentViewAnchor = contentView.rightAnchor
+        let bottomContentViewAnchor = contentView.bottomAnchor
+        
         NSLayoutConstraint.activate([
-            hotelImageView.topAnchor.constraint(equalTo: topAnchor),
-            hotelImageView.leftAnchor.constraint(equalTo: leftAnchor),
-            hotelImageView.rightAnchor.constraint(equalTo: rightAnchor),
+            hotelImageView.topAnchor.constraint(equalTo: topContentViewAnchor),
+            hotelImageView.leftAnchor.constraint(equalTo: leftContentViewAnchor),
+            hotelImageView.rightAnchor.constraint(equalTo: rightContentViewAnchor),
             hotelImageView.heightAnchor.constraint(equalToConstant: 200),
             
             titleLabel.topAnchor.constraint(equalTo: hotelImageView.bottomAnchor, constant: 10),
             titleLabel.leftAnchor.constraint(equalTo: hotelImageView.leftAnchor),
             titleLabel.rightAnchor.constraint(equalTo: hotelImageView.rightAnchor),
-            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomContentViewAnchor),
             titleLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 40),
+            
+            popupView.topAnchor.constraint(equalTo: topContentViewAnchor, constant: 10),
+            popupView.leftAnchor.constraint(equalTo: leftContentViewAnchor, constant: 10),
+            popupView.rightAnchor.constraint(equalTo: rightContentViewAnchor, constant: -10),
+            popupView.bottomAnchor.constraint(equalTo: bottomContentViewAnchor, constant: -10),
+            
+            popupView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            popupView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 10),
+            popupView.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -10),
+            
+            popupTextView.topAnchor.constraint(equalTo: popupView.topAnchor, constant: 10),
+            popupTextView.leftAnchor.constraint(equalTo: popupView.leftAnchor, constant: 10),
+            popupTextView.rightAnchor.constraint(equalTo: popupView.rightAnchor, constant: -10),
+            popupTextView.heightAnchor.constraint(greaterThanOrEqualToConstant: 150),
+            
+            closeButton.topAnchor.constraint(equalTo: popupTextView.bottomAnchor, constant: 10),
+            closeButton.centerXAnchor.constraint(equalTo: popupView.centerXAnchor),
+            closeButton.bottomAnchor.constraint(equalTo: popupView.bottomAnchor),
+            closeButton.widthAnchor.constraint(equalToConstant: 25),
+            closeButton.heightAnchor.constraint(equalToConstant: 25),
         ])
     }
     
@@ -91,5 +152,22 @@ class ImageDataCollectionViewCell: UICollectionViewCell {
         
         // Set title label
         titleLabel.text = title?.isEmpty == true ? "實景示意圖" : (title ?? "很抱歉，此旅店尚未提供圖檔哦~")
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTitleLabelTap(_:)))
+        titleLabel.isUserInteractionEnabled = true
+        titleLabel.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func handleTitleLabelTap(_ gesture: UITapGestureRecognizer) {
+        guard let titleLabelText = titleLabel.text, titleLabelText.count > 18 else {
+            return
+        }
+        popupView.isHidden = false
+        popupTextView.text = titleLabelText
+        closeButton.addTarget(self, action: #selector(closePopup(_:)), for: .touchUpInside)
+    }
+    
+    @objc func closePopup(_ sender: UIButton) {
+        popupView.isHidden = true
     }
 }
