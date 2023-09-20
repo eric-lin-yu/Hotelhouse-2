311fa10b1d9218aa51b8299b0a127326c007b933
EricLin <zl2408399@gmail.com>
2023-09-20 15:49:19 +0800

    feat: add CollectionsViewController
    TODO: cell UI

--- 寶島旅社/收藏/CollectionsViewController.swift
+++ 寶島旅社/收藏/CollectionsViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class CollectionsViewController: UIViewController {
-    
+    private var hotelDataModel: [Hotels] = []
     // constraint Spacing
     private let spacing: CGFloat = 20
-    private let topSpacing: CGFloat = 30
-    private let bottomSpacing: CGFloat = 35
-    private let btnHeight: CGFloat = 50
+    private let innerLayerSpacing: CGFloat = 10
+    private let searchiconBtnSize: CGFloat = 24
+    private let segmentedControlSize: CGFloat = 40
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -25,21 +25,94 @@ class CollectionsViewController: UIViewController {
     }
     
     //MARK: - UI
-    private let bgImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "<#imageName#>")
-        imageView.contentMode = .scaleToFill
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
+    private let searchView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        view.layer.cornerRadius = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let searchTextFiled: UITextField = {
+        let textField = UITextField()
+        textField.textColor = .white
+        textField.backgroundColor = .sageGreen
+        textField.font = .systemFont(ofSize: 17)
+        
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.white,
+            .font: UIFont.systemFont(ofSize: 17)
+        ]
+        let attributedPlaceholder = NSAttributedString(string: "Search", attributes: attributes)
+        textField.attributedPlaceholder = attributedPlaceholder
+
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private let searchiconBtn: UIButton = {
+        let button = UIButton()
+        let image = UIImage(named: "iconSearch")
+        button.setImage(image, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let segmentedControl: UISegmentedControl = {
+        let segmentedControl = UISegmentedControl()
+        segmentedControl.insertSegment(withTitle: "列表", at: 0, animated: false)
+        segmentedControl.insertSegment(withTitle: "地圖", at: 1, animated: false)
+        
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.backgroundColor = .sageGreen
+        
+        // 置中
+        let paragraphStyle = NSMutableParagraphStyle()
+           paragraphStyle.alignment = .center
+        
+        let normalTextAttributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.white,
+            .font: UIFont.systemFont(ofSize: 17),
+            .paragraphStyle: paragraphStyle
+        ]
+        
+        let selectedTextAttributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.orangeRed,
+            .font: UIFont.systemFont(ofSize: 17),
+            .paragraphStyle: paragraphStyle
+        ]
+        
+        segmentedControl.setTitleTextAttributes(normalTextAttributes, for: .normal)
+        segmentedControl.setTitleTextAttributes(selectedTextAttributes, for: .selected)
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        return segmentedControl
+    }()
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.separatorStyle = .none
+        tableView.showsVerticalScrollIndicator = false
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
     }()
     
     //MARK: - setup
     private func setupViews() {
-        
+        view.backgroundColor = .white
         let viewsToAdd: [UIView] = [
-            //...
+            searchView,
+            segmentedControl,
+            tableView,
         ]
         viewsToAdd.forEach { view.addSubview($0) }
+        
+        let viewsToAddSearchView: [UIView] = [
+            searchTextFiled,
+            searchiconBtn,
+        ]
+        viewsToAddSearchView.forEach { searchView.addSubview($0) }
     }
     
     private func setupConstraint() {
@@ -49,24 +122,73 @@ class CollectionsViewController: UIViewController {
         let bottomSafeArea = view.safeAreaLayoutGuide.bottomAnchor
         
         NSLayoutConstraint.activate([
+            searchView.topAnchor.constraint(equalTo: topSafeArea, constant: spacing),
+            searchView.leftAnchor.constraint(equalTo: leftSafeArea, constant: spacing),
+            searchView.rightAnchor.constraint(equalTo: rightSafeArea, constant: -spacing),
             
+            searchTextFiled.topAnchor.constraint(equalTo: searchView.topAnchor),
+            searchTextFiled.leftAnchor.constraint(equalTo: searchView.leftAnchor),
+            searchTextFiled.rightAnchor.constraint(equalTo: searchView.rightAnchor),
+            searchTextFiled.bottomAnchor.constraint(equalTo: searchView.bottomAnchor),
+            
+            searchiconBtn.topAnchor.constraint(equalTo: searchView.topAnchor, constant: innerLayerSpacing),
+            searchiconBtn.rightAnchor.constraint(equalTo: searchView.rightAnchor, constant: -innerLayerSpacing),
+            searchiconBtn.bottomAnchor.constraint(equalTo: searchView.bottomAnchor, constant: -innerLayerSpacing),
+            searchiconBtn.centerYAnchor.constraint(equalTo: searchView.centerYAnchor),
+            searchiconBtn.heightAnchor.constraint(equalToConstant: searchiconBtnSize),
+            searchiconBtn.widthAnchor.constraint(equalToConstant: searchiconBtnSize),
+        
+            segmentedControl.topAnchor.constraint(equalTo: searchView.bottomAnchor, constant: innerLayerSpacing),
+            segmentedControl.leftAnchor.constraint(equalTo: searchView.leftAnchor),
+            segmentedControl.rightAnchor.constraint(equalTo: searchView.rightAnchor),
+            segmentedControl.heightAnchor.constraint(equalToConstant: segmentedControlSize),
+            
+            tableView.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: innerLayerSpacing),
+            tableView.leftAnchor.constraint(equalTo: leftSafeArea),
+            tableView.rightAnchor.constraint(equalTo: rightSafeArea),
+            tableView.bottomAnchor.constraint(equalTo: bottomSafeArea),
         ])
+    }
+    
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+//        let useCells = []
+//        useCells.forEach {
+//            tableView.register($0.self, forCellReuseIdentifier: $0.storyboardIdentifier)
+//        }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
         setupConstraint()
+        setupTableView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: animated)
+        if let realmDataModels = RealmManager.shard?.getHotelDataModelsFromRealm() {
+            self.hotelDataModel = realmDataModels
+        }
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
+}
 
+//MARK: - TableView
+extension CollectionsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return hotelDataModel.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
 }
